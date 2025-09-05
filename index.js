7e40fff50029974004e9d8a7662a6181b074adb9
Bijan Jadhavji <12b.jad@gmail.com>
2025-09-05 17:56:40 -0500

    updated js

---
+++ index.js
@@ -0,0 +1,13 @@
+let code1;
+
+code1 = 7 + 3;
+
+let code2 = (100 - 20) / 2;
+
+let code3 = 100 % 61;
+
+let message = "The vault has been secured. The combination is:";
+
+let codeA = code1 + "-" + code2 + "-" + code3;
+
+let codeB = `${code1}-${code2}-${code3}`;
